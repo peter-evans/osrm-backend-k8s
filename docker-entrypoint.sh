05682d9fd6178e31a45ecff57262aaaf01d7967f
peter-evans <peter-evans@users.noreply.github.com>
2016-12-13 19:29:25 +0900

    Refactored entrypoint to use environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-DATA_PATH=${DATA_PATH:="/osrm-data"}
+
+if [ "$OSRM_MODE" != "CREATE" ] && [ "$OSRM_MODE" != "RESTORE" ]; then
+    # Default to CREATE
+    OSRM_MODE="CREATE"
+fi
+
+# Defaults
+OSRM_DATA_PATH=${OSRM_DATA_PATH:="/osrm-data"}
+OSRM_DATA_LABEL=${OSRM_DATA_LABEL:="data"}
+OSRM_GRAPH_PROFILE=${OSRM_GRAPH_PROFILE:="car"}
+OSRM_PBF_URL=${OSRM_PBF_URL:="http://download.geofabrik.de/asia/maldives-latest.osm.pbf"}
+# Google Storage variables
+OSRM_SA_KEY_PATH=${OSRM_SA_KEY_PATH:=""}
+OSRM_PROJECT_ID=${OSRM_PROJECT_ID:=""}
+OSRM_GS_BUCKET=${OSRM_GS_BUCKET:=""}
+
 
 _sig() {
   kill -TERM $child 2>/dev/null
@@ -7,39 +22,47 @@ _sig() {
 trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
 
-if [ $# -eq 6 ]; then
-
+if [ "$OSRM_MODE" == "CREATE" ]; then
+    
     # Set the graph profile (car/bicycle/foot)
-    mv $5.lua profile.lua
+    mv $OSRM_GRAPH_PROFILE.lua profile.lua
 
     # Retrieve the PBF file
-    curl $6 > $DATA_PATH/$1.osm.pbf
+    curl $OSRM_PBF_URL --create-dirs -o $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf
     
     # Build the graph
-    ./osrm-extract $DATA_PATH/$1.osm.pbf
-    ./osrm-contract $DATA_PATH/$1.osrm
+    ./osrm-extract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf
+    ./osrm-contract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm
 
-    # Activate the service account to access storage
-    gcloud auth activate-service-account --key-file $2
-    # Set the Google Cloud project ID
-    gcloud config set project $3
-
-    # Copy the graph data to storage
-    gsutil -m cp $DATA_PATH/*.osrm* $4/$1
+    if [ ! -z "$OSRM_SA_KEY_PATH" ] && [ ! -z "$OSRM_PROJECT_ID" ] && [ ! -z "$OSRM_GS_BUCKET" ]; then
     
-elif [ $# -eq 4 ]; then
+        # Activate the service account to access storage
+        gcloud auth activate-service-account --key-file $OSRM_SA_KEY_PATH
+        # Set the Google Cloud project ID
+        gcloud config set project $OSRM_PROJECT_ID
 
-    # Activate the service account to access storage
-    gcloud auth activate-service-account --key-file $2
-    # Set the Google Cloud project ID
-    gcloud config set project $3
+        # Copy the graph to storage
+        gsutil -m cp $OSRM_DATA_PATH/*.osrm* $OSRM_GS_BUCKET/$OSRM_DATA_LABEL
 
-    # Copy the graph from storage
-    gsutil -m cp $4/$1/*.osrm* $DATA_PATH
+    fi
+    
+else
+
+    if [ ! -z "$OSRM_SA_KEY_PATH" ] && [ ! -z "$OSRM_PROJECT_ID" ] && [ ! -z "$OSRM_GS_BUCKET" ]; then
+
+        # Activate the service account to access storage
+        gcloud auth activate-service-account --key-file $OSRM_SA_KEY_PATH
+        # Set the Google Cloud project ID
+        gcloud config set project $OSRM_PROJECT_ID
+
+        # Copy the graph from storage
+        gsutil -m cp $OSRM_GS_BUCKET/$OSRM_DATA_LABEL/*.osrm* $OSRM_DATA_PATH
+
+    fi
     
 fi
 
 # Start serving requests
-./osrm-routed $DATA_PATH/$1.osrm --max-table-size 8000 &
+./osrm-routed $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm --max-table-size 8000 &
 child=$!
 wait "$child"
